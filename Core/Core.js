d32686e5ef34f52095b397b16339b41bda06e515
ZaksDev_4ever <272747337+z4kkiexe-bit@users.noreply.github.com>
2026-09-12 17:17:02 +0800

    prefix: fix module not found

--- Core/Core.js
+++ Core/Core.js
@@ -2,7 +2,7 @@ import "../Whatsapp/Whatsapp.js"
 import express from "express"
 import { spawn } from "child_process" 
 import "dotenv/config"
-import { STT } from "./audioPlayer.js"
+
 
 const app = express()
 export let resSTT;
